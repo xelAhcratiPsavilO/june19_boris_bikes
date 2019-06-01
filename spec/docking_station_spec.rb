0d8e1f087b65c9d5baf5fc3dea345b9d04e467cb
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2019-06-01 19:15:52 +0100

    Update test for Bike method working?

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,4 +4,9 @@ describe DockingStation do
 
   it { is_expected.to respond_to :release_bike }
 
+  it "releases bikes that work" do
+    bike = subject.release_bike
+    expect(bike.working?).to be true
+  end
+
 end
